f85643c82e575df96b31c41eeae987b5b9f03995
“MichaelWen <michaelwen.dev@gmail.com>
2016-11-19 18:57:48 +0000

    Render New Post at Main

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.cupcake_list, name="cupcake_list"),
+	url(r'^$', views.getPostList, name="post_list"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from . import models
 from .models import Cupcake
+from .models import Post
+
 
 def cupcake_list(request):
 	cakes = Cupcake.objects.all().order_by('-createdAt')
 	context = {"cakes": cakes}
+	return render(request,"menu/list.html",context)
+
+def getPostList(request):
+	posts = Post.objects.all().order_by('-createdAt')
+	context = {"posts": posts}
 	return render(request,"menu/list.html",context)
 
 # Create your views here.
