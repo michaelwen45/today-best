687a44d82d3f15fe90e456514e3c8d1396582802
“MichaelWen <michaelwen.dev@gmail.com>
2016-11-20 07:40:24 +0000

    Show Detail Page when Like is clicked

--- menu/urls.py
+++ menu/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.getPostList, name="post_list"),
+	url(r'^post/(?P<pk>\d+)/$',views.post_detail,name="post_detail")
 ]
--- menu/views.py
+++ menu/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from . import models
 from .models import Cupcake
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
-
-def cupcake_list(request):
-	cakes = Cupcake.objects.all().order_by('-createdAt')
-	context = {"cakes": cakes}
-	return render(request,"menu/list.html",context)
 
 def getPostList(request):
 	posts = Post.objects.all().order_by('-createdAt')
 	context = {"posts": posts}
 	return render(request,"menu/list.html",context)
 
-# Create your views here.
+def	post_detail(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	context = {"post": post}
+	return render(request, "menu/detail.html", context)
